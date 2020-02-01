ff2cb5beaa22ced818741c14de1a1d1d86daab3e
katungi <dankatdennis@gmail.com>
2020-02-01 16:58:49 +0300

    new pages controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -56,7 +56,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully removed.' }
       format.json { head :no_content }
     end
   end
